7c28840a5e61287babb0f8816d16bbe20c86b0a9
Matheus França <matfbs1@gmail.com>
2022-10-07 16:42:51 -0300

    Requisito 1

---
+++ src/redux/actions/index.js
@@ -0,0 +1,18 @@
+export const SAVE_PLAYER = 'SAVE_PLAYER';
+export const SAVE_ASSERTIONS = 'SAVE_ASSERTIONS';
+export const CALCULATE_SCORE = 'CALCULATE_SCORE';
+
+export const savePlayer = (payload) => ({
+  type: SAVE_PLAYER,
+  payload,
+});
+
+export const saveAssertions = (payload) => ({
+  type: SAVE_ASSERTIONS,
+  payload,
+});
+
+export const calculateScore = (payload) => ({
+  type: CALCULATE_SCORE,
+  payload,
+});
